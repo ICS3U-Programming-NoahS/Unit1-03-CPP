687dd447f457827bae3e27271b16e2d4d8d01a12
Noah Smith <noah.smith@stu.ocsb.ca>
2023-09-17 17:37:37 +0000

    Created helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Noah Smith All rights reserved.
+//
+// Created by Noah Smith
+// Date: Sept. 17th 2023
+// This program displays "Hello, World! With proper style, using C++."
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World! With proper style, using C++."<< std::endl;
+    std::cout << "C++ is a bit more complicated than Python!" << std::endl;
+}
